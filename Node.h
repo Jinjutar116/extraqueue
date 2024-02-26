9530b2b737ae295f2061ef7008c3a09ffa2afba2
Jinjutar Sookprasert <156405233+Jinjutar116@users.noreply.github.com>
2024-02-26 06:31:54 +0000

    add

--- Node.h
+++ Node.h
@@ -16,26 +16,29 @@ struct node
 typedef struct node Node;
 typedef struct node* NodePtr;
 
-
 void enqueue(NodePtr * head, NodePtr* tail, int x){
-  Node* new_node=(NodePtr) malloc(sizeof(Node));
-if(new_node){ 
-    /* Finish queue*/
- }
+  NodePtr new_node = (NodePtr)malloc(sizeof(Node));
+  if(new_node){ 
+    new_node->data = x;
+    new_node->nextPtr = NULL;
+    if(*head == NULL) *head = new_node;
+    else(*tail)->nextPtr = new_node;
+    *tail = new_node;
+  }
+  else printf("Not enough memmory");
 }
 
-
 int dequeue(NodePtr* head, NodePtr* tail){
-  NodePtr t=*head;
-   if(t){
-   int value= t->data;
-   /* Finish dequeue*/
-       
-       
-   return value;
-   }
-   printf("Empty queue");
-   return 0;
+  NodePtr t = *head;
+  if(t){
+    int value= t->data;
+    *head = t->nextPtr;
+    free(t);
+    if(*head == NULL) *tail = NULL;           
+    return value;
+  }
+  printf("Empty queue");
+  return 0;
 }
 
 
--- main.c
+++ main.c
@@ -6,26 +6,21 @@
 #include "Queue.h"
 
 int main(int argc , char **argv) {
-  NodePtr headPtr=NULL;
-   NodePtr tailPtr=NULL;
-/* For struct Queue
   Queue  q;
-   q. headPtr=NULL;
-   q.tailPtr=NULL;
-   q.size=0;
-*/
-   int i,x;
-   
+  main_init(&q);
+  int i,x; 
 
- for(i=1;i<argc;i++){
-        if(strcmp(argv[i],"x")==0){
-            x=dequeue(&headPtr,&tailPtr);
-            printf("dequeing %d\n",x);
-        }
-        else {
-       enqueue_struct(&headPtr,&tailPtr, atoi(argv[i]));
-           
-        }
- }
+  for(i = 1; i < argc; i++){
+    if(strcmp(argv[i], "x") == 0) {
+      if(q.size != 0) {
+        x = dequeue_struct(&q);
+        printf("dequeing %d\n", x);
+      }
+      else dequeue_struct(&q);
+    }
+    else {
+      enqueue_struct(&q, atoi(argv[i]));
+    }
+  }
   return 0;
 }
